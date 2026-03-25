8582b6132617b7e6c13c9653df13c3acc9485ffd
amarifansari <64271958+amarifansari@users.noreply.github.com>
2026-03-25 21:35:33 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -9,6 +9,7 @@
 
     function onReady(smart)  {
       alert('here');
+      alert(smart.hasOwnProperty('patient'));
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
