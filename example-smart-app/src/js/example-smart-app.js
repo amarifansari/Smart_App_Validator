7b6d28c87580345cf8ef9f08d4e48294cddb9747
amarifansari <64271958+amarifansari@users.noreply.github.com>
2026-03-26 14:18:08 +0000

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -28,7 +28,7 @@
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
-          smart.
+         
           var gender = patient.gender;
 
           var fname = '';
