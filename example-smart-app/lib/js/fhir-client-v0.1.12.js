e754d2825d7102de12b21aa3ef564501c91b7372
amarifansari <64271958+amarifansari@users.noreply.github.com>
2026-04-01 16:47:03 +0100

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -140,6 +140,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	        var Path = url.Path;
 	        var BaseUrl = Path(cfg.baseUrl);
+			alert(BaseUrl);
 	        var resourceTypePath = BaseUrl.slash(":type || :resource.resourceType");
 	        var searchPath = resourceTypePath;
 	        var resourceTypeHxPath = resourceTypePath.slash("_history");
