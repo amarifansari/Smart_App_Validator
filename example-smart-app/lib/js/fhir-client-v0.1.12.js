a07e1fb0a197b13fa90f5d94ea2fe960b9ad0426
amarifansari <64271958+amarifansari@users.noreply.github.com>
2026-04-01 17:02:24 +0100

    Update fhir-client-v0.1.12.js

--- example-smart-app/lib/js/fhir-client-v0.1.12.js
+++ example-smart-app/lib/js/fhir-client-v0.1.12.js
@@ -154,6 +154,37 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	        var $Paging = Middleware(query.$Paging);
 
+			var api = decorate({
+    conformance: GET.and(BaseUrl.slash("metadata")).end(http),
+    document: POST.and(BaseUrl.slash("Document")).end(http),
+    profile:  GET.and(BaseUrl.slash("Profile").slash(":type")).end(http),
+    transaction: POST.and(BaseUrl).end(http),
+    history: GET.and(BaseUrl.slash("_history")).and($Paging).end(http),
+    typeHistory: GET.and(resourceTypeHxPath).and($Paging).end(http),
+    resourceHistory: GET.and(resourceHxPath).and($Paging).end(http),
+    read: GET.and(pt.$WithPatient).and(resourcePath).end(http),
+    vread: GET.and(vreadPath).end(http),
+    "delete": DELETE.and(resourcePath).and(ReturnHeader).end(http),
+    create: POST.and(resourceTypePath).and(ReturnHeader).end(http),
+    validate: POST.and(resourceTypePath.slash("_validate")).end(http),
+    search: GET.and(resourceTypePath).and(pt.$WithPatient).and(query.$SearchParams).and($Paging).end(http),
+    update: PUT.and(resourcePath).and(ReturnHeader).end(http),
+    nextPage: GET.and(bundle.$$BundleLinkUrl("next")).end(http),
+    prevPage: GET.and(bundle.$$BundleLinkUrl("prev")).end(http),
+    resolve: GET.and(refs.resolve).end(http)
+}, adapter);
+
+Object.keys(api).forEach(function(name) {
+    var fn = api[name];
+    if (typeof fn !== "function") return;
+
+    api[name] = function() {
+        alert("Calling FHIR method: " + name);
+        return fn.apply(this, arguments);
+    };
+});
+
+
 	        return decorate({
 	            conformance: GET.and(BaseUrl.slash("metadata")).end(http),
 	            document: POST.and(BaseUrl.slash("Document")).end(http),
